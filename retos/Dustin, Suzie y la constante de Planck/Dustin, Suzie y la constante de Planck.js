d69ba4c422b0209831b5c9c1e911caa35fc6ebf4
thamaragerigr <thamaragerigr@gmail.com>
2020-06-18 23:53:35 +0200

    Dustin, Suzie y la constante de Planck

---
+++ retos/Dustin, Suzie y la constante de Planck/Dustin, Suzie y la constante de Planck.js
@@ -0,0 +1,7 @@
+const constanteDePlanck = 6.62607015;
+const input = document.querySelector('.input').value;
+
+const esLaConstanteCorrecta = function() {
+    if (constanteDePlanck === input) console.log('si')
+    console.log(input)
+}
--- retos/retos.md
+++ retos/retos.md
@@ -1,3 +1,4 @@
 # Retos Desprogramadas
 
 - [01 - La casa de papel](https://didacticode.com/reto-la-casa-de-papel/)
+- [02 - Dustin, Suzie y la constante de Planck](https://didacticode.com/reto-stranger-things/)
