a2b71ada845b0d8d2e7fe133e216c68a99bc533b
thamaragerigr <thamaragerigr@gmail.com>
2020-06-23 23:43:10 +0200

    reto 02 terminado

--- retos/Dustin, Suzie y la constante de Planck/Dustin, Suzie y la constante de Planck.js
+++ retos/Dustin, Suzie y la constante de Planck/Dustin, Suzie y la constante de Planck.js
@@ -1,7 +1,12 @@
 const constanteDePlanck = 6.62607015;
 const input = document.querySelector('.input').value;
 
+
+//Mid-Level 
 const esLaConstanteCorrecta = function() {
-    if (constanteDePlanck === input) console.log('si')
-    console.log(input)
+    if (constanteDePlanck === input) return true
+    return false
 }
+
+//Pro
+const esLaConstanteCorrecta2 = () => (constanteDePlanck === input) ? true : false
