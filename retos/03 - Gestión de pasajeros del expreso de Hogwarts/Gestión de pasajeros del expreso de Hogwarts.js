86bb194ae34a10ad0b0c978c65c9b6c4dddd6705
thamaragerigr <thamaragerigr@gmail.com>
2020-06-26 23:50:01 +0200

    reto harry potter

--- retos/03 - Gestión de pasajeros del expreso de Hogwarts/Gestión de pasajeros del expreso de Hogwarts.js
+++ retos/03 - Gestión de pasajeros del expreso de Hogwarts/Gestión de pasajeros del expreso de Hogwarts.js
@@ -1,3 +1,7 @@
-function controlDePasajeros(arr) {
-    arr.reduce(function(a, b) { return a + b; }, 0);
-}
+const numeroDePasajeros = [[7,0],[8,2],[9,5],[3,1]]
+
+// function controlDePasajeros(arr) {
+//    return arr.reduce((acumulador, [arr[0], arr[1]]) { return acumulador + b })
+// }
+
+const controlDePasajeros = numeroDePasajeros => numeroDePasajeros.reduce((ac, [s,b]) => ac + s + b, 0)
